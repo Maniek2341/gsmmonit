97146c37e2825b251a3dd7db86e77866b724c612
Maniek2341 <maniek4123@onet.pl>
2022-07-02 09:34:12 +0200

    Commit

--- website/views/active_language.py
+++ website/views/active_language.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import translate_url
 from django.utils.http import url_has_allowed_host_and_scheme
-from django.utils.translation import (
-    LANGUAGE_SESSION_KEY, check_for_language
-)
+from django.utils.translation import check_for_language
+
+LANGUAGE_SESSION_KEY: str = '_language'
 
 
 def active_language(request, lang_code=settings.LANGUAGE_CODE):
